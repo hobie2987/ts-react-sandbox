c2e013995c149b369d1cb5a14c1822dfd7f8a12f
hobie2987 <hobie2987@gmail.com>
2022-06-01 12:29:15 -0400

    feat(components): add to exports

--- src/modules/shop/components/index.tsx
+++ src/modules/shop/components/index.tsx
@@ -1,4 +1,15 @@
 import Button from './button/Button';
+import Loading from './loading/Loading';
+import Price from './price/Price';
 import ProductTile from './product-tile/ProductTile';
+import Quantity from './quantity/Quantity';
+import OrderSummary from "./order-summary/OrderSummary";
 
-export { Button, ProductTile };
+export {
+    Button,
+    Loading,
+    OrderSummary,
+    Price,
+    ProductTile,
+    Quantity
+};
