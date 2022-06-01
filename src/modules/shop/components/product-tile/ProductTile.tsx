1808def3d9dc6a2f570cf6bc98f540d5c1808b65
hobie2987 <hobie2987@gmail.com>
2022-06-01 12:24:37 -0400

    feat(product-tile): add/remove items to cart

--- src/modules/shop/components/product-tile/ProductTile.tsx
+++ src/modules/shop/components/product-tile/ProductTile.tsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Button from '../button/Button';
 import Quantity from '../quantity/Quantity';
 import Price from "../price/Price";
 import Product from "../../../../models/product";
 import './ProductTile.scss';
+import { Cart } from "../../../../services";
 
 
 export default function ProductTile(props: { product: Product }) {
     const [selected, setSelected] = useState(false)
     const [product] = useState(props.product)
 
-    const toggleSelected = () => {
-        setSelected(!selected);
-    }
+    const addToCart = (qty?: number) => Cart.add(product.sku, qty).then((items) => {
+        setSelected(items.has(product.sku));
+    });
+
+
+    const removeFromCart = () => Cart.remove(product.sku).then((items) => {
+        setSelected(items.has(product.sku));
+    });
+
+    useEffect(() => {
+        Cart.getItems().then((items) => {
+            // const item = items.get(product.sku);
+            setSelected(items.has(product.sku));
+            // initialize quantity
+        })
+    });
+
 
     return (
         <div className="Tile">
@@ -24,15 +39,17 @@ export default function ProductTile(props: { product: Product }) {
 
             <div className="amount">
                 <Quantity min={1}
-                          zero={toggleSelected}
-                          disabled={!selected} />
+                          zero={removeFromCart}
+                          disabled={!selected}
+                          onChange={addToCart}/>
                 <Price value={product.price} />
             </div>
 
 
             <Button label={selected ? 'Selected' : 'Select'}
                     selected={selected}
-                    action={toggleSelected}/>
+                    disabled={selected}
+                    action={addToCart}/>
         </div>
     );
 }
