e2772e6a31263fc0f9dd6394d598b0cb67f5b06c
hobie2987 <hobie2987@gmail.com>
2022-06-01 12:29:58 -0400

    feat(shop): add swr to retrieve cameras

--- src/modules/shop/Shop.tsx
+++ src/modules/shop/Shop.tsx
@@ -1,29 +1,32 @@
-import {useEffect, useState} from "react";
-import { ProductTile } from './components';
-import { ProductsService } from "../../services";
 import './Shop.scss';
+import { Loading, ProductTile, OrderSummary } from './components';
+import { CATALOG } from "../../models/api";
+import { GET } from "../../services";
+import useSWR from 'swr';
 import Product from "../../models/product";
 
 export default function Shop() {
-    const [cameras, setCameras] = useState([]);
+    const { data, error } = useSWR(CATALOG, GET)
+    const cameras: Product[] = data?.cameras ? Object.values(data.cameras) : [];
 
-    useEffect(() => {
-        ProductsService.getCameras()
-            .then((cameras: Product[]) => {
-                setCameras(cameras);
-            })
-            .catch((error) => {
-                console.error('Error fetching products', error);
-            })
-    }, [])
+    if (error) {
+        return <div>Oops, something broke</div>
+    }
+
+    if (!cameras.length) {
+        return <Loading />
+    }
 
     return (
         <div className="Shop">
-            <h1>Shopping!</h1>
-            <div className="tiles">
-                {cameras.map(camera => (
-                    <ProductTile key={camera.id} product={camera} />
-                ))}
+            <h1>Buy Matterport</h1>
+            <div className="content">
+                <div className="tiles">
+                    {cameras.map(camera => (
+                        <ProductTile key={camera.id} product={camera} />
+                    ))}
+                </div>
+                <OrderSummary />
             </div>
         </div>
     );
