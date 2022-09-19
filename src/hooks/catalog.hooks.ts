45a723edd60f3c2c3372e3b3acc56080591af9e6
hobie2987 <hobie2987@gmail.com>
2022-09-18 20:53:08 -0400

    fix catalog hooks

--- src/hooks/catalog.hooks.ts
+++ src/hooks/catalog.hooks.ts
@@ -2,26 +2,27 @@ import { useEffect, useState } from "react";
 import { ProductsCatalog } from "../namespaces/products-catalog";
 import _ from 'lodash';
 import Product from "../models/product";
-import {Catalog} from "../models/catalog";
+// import {Catalog} from "../models/catalog";
 
-export const useCatalog = (): [Catalog] => {
-    const [catalog, setCatalog] = useState(undefined);
+// export const useCatalog = (): [Catalog] => {
+//     const [catalog, setCatalog] = useState(undefined);
+//
+//     useEffect(() => {
+//         ProductsCatalog.getCatalog().subscribe((catalog) => {
+//             setCatalog(() => catalog)
+//         })
+//     });
+//     return [catalog];
+// }
+
+export const useProducts = (type): [Product[]] => {
+    const [products, setProducts] = useState(() => [])
 
     useEffect(() => {
         ProductsCatalog.getCatalog().subscribe((catalog) => {
-            setCatalog(() => catalog)
+            setProducts(() => Object.values(_.get(catalog, type)) || [])
         })
-    });
-    return [catalog];
-}
-
-export const useProducts = (type): [any[]] => {
-    const [catalog] = useCatalog();
-    const [products, setProducts] = useState(() => [])
-
-    setProducts(() => {
-        return catalog ? Object.values(_.get(catalog, type)) : []
-    })
+    }, [type]);
 
     return [products];
 }
