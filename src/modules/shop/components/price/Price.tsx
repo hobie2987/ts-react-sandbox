77240e7a436ae28dfab37f53e0fb82050b84de0c
hobie2987 <hobie2987@gmail.com>
2022-09-18 20:54:52 -0400

    value as string or number

--- src/modules/shop/components/price/Price.tsx
+++ src/modules/shop/components/price/Price.tsx
@@ -1,12 +1,12 @@
 import './Price.scss';
 import { useLocale, useProfile } from "../../../../hooks";
 
-export default function Price(props: { value: number | undefined }) {
+export default function Price(props: { value: string | number | undefined }) {
         const [locale] = useLocale();
         const [currency] = useProfile('currency', 'USD');
 
         function format(): string {
-            return new Intl.NumberFormat(locale, {style: 'currency', currency }).format(props.value);
+            return new Intl.NumberFormat(locale, {style: 'currency', currency }).format(Number(props.value));
         }
 
         return (
