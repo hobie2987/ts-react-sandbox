ba3df82fe99dbf1f7da4500f182bc5438a31a728
hobie2987 <hobie2987@gmail.com>
2022-09-25 18:39:04 -0400

    item count and remove individual item

--- src/modules/shop/components/order-summary/OrderSummary.tsx
+++ src/modules/shop/components/order-summary/OrderSummary.tsx
@@ -3,54 +3,68 @@ import Button from "../button/Button";
 import Price from "../price/Price";
 import { OrderSession } from "../../../../namespaces/order-session";
 import { useCart, useContent, useSession } from "../../../../hooks";
-import {ExpandMore, ShoppingCart} from '@mui/icons-material';
-import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { Close, ExpandMore, ShoppingCart } from '@mui/icons-material';
+import { Accordion, AccordionDetails, AccordionSummary, Badge, Divider, IconButton } from "@mui/material";
 
 export default function OrderSummary() {
     const [content] = useContent();
     const [cart] = useCart();
     const [session] = useSession();
+    const itemCount = cart.items.reduce((count, item) => count + item.quantity, 0);
 
     function clear() {
         OrderSession.clearCart();
+    }
+
+    function removeItem(sku: string): void {
+        OrderSession.removeItem(sku);
     }
 
     return(
         <Accordion className="OrderSummary" defaultExpanded={true} >
 
             <AccordionSummary expandIcon={<ExpandMore />} >
-                <ShoppingCart />
+                <Badge color="primary"
+                    badgeContent={itemCount}>
+                    <ShoppingCart />
+                </Badge>
+
                 <span>{ content['ORDER_SUMMARY.HEADING'] }</span>
             </AccordionSummary>
 
             <AccordionDetails>
                 {cart.items.length < 1 &&
-                    <div>
-                        <div>{ content['ORDER_SUMMARY.EMPTY_CART'] }</div>
-                    </div>
+                    <div className="empty-cart">{ content['ORDER_SUMMARY.EMPTY_CART'] }</div>
                 }
 
                 {cart.items.map(item => (
                     <div key={item.sku} className="cart-item">
-                        <div>
+                        <div className="item-desc">
                             <div className="item-name">{session.products[item.sku].name}</div>
-                            <div className="item-qty">Qty {item.quantity}</div>
+                            <div className="item-price">
+                                <Price value={session.products[item.sku].price * item.quantity} />
+                            </div>
+                            <IconButton className="item-remove" onClick={() => removeItem(item.sku)}>
+                                <Close></Close>
+                            </IconButton>
                         </div>
-                        <div className="item-price">
-                            <Price value={session.products[item.sku].price * item.quantity} />
-                        </div>
+                        <div className="item-qty">Qty {item.quantity}</div>
                     </div>
                 ))}
 
+                <Divider></Divider>
+
                 <div className="cart-item">
-                    <div className="item-name">{ content["ORDER_SUMMARY.TODAYS_TOTAL"] }</div>
-                    <div className="item-price">
-                        <Price value={cart.total} />
+                    <div className="item-desc">
+                        <div className="item-name">{ content["ORDER_SUMMARY.TODAYS_TOTAL"] }</div>
+                        <div className="item-price">
+                            <Price value={cart.camera_total} />
+                        </div>
                     </div>
                 </div>
 
                 {cart.items.length > 0 &&
-                <Button label={ content["ORDER_SUMMARY.BUTTON.CLEAR_CART"] } action={clear} />
+                    <Button label={ content["ORDER_SUMMARY.BUTTON.CLEAR_CART"] } action={clear} />
                 }
 
                 <Button label={ content["ORDER_SUMMARY.BUTTON.CHECK_OUT"] } />
